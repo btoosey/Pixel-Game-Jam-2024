8e22e92be02c0ed404784497af3a2cd40d1786ed
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2024-05-14 11:39:12 +0100

    ADD: Bubble spawner basic functionality
    Bubble spawner now instantiates a bubble of a single type

--- scenes/bubble_spawner.gd
+++ scenes/bubble_spawner.gd
@@ -2,18 +2,32 @@ extends Node2D
 
 @export var speed: float = 1.1
 @export var bubble : PackedScene = preload("res://scenes/bubble.tscn")
+@onready var bubbles = $"../Bubbles"
+var can_spawn_bubble = true
+@onready var timer = $Timer
 
 func _physics_process(_delta):
 	if Input.is_action_pressed("move_left"):
-		if position.x <= -50:
+		if position.x <= 100:
 			return
 		else:
 			position.x -= speed
+
 	if Input.is_action_pressed("move_right"):
-		if position.x >= 50:
+		if position.x >= 200:
 			return
 		else:
 			position.x += speed
-	if Input.is_action_pressed("spawn_bubble"):
+
+	if Input.is_action_just_pressed("spawn_bubble"):
+		if not can_spawn_bubble:
+			return
+
 		var _bubble = bubble.instantiate()
+		can_spawn_bubble = false
+		bubbles.add_child(_bubble)
 		_bubble.position = position
+		timer.start()
+
+func _on_timer_timeout():
+	can_spawn_bubble = true
